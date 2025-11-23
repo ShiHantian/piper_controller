de9f49b71567ed3f764f9e370812d4a90f41efa3
shihantian <htshi1011@gmail.com>
2025-11-23 15:43:30 +0800

    init test_angle_limit with test_safe_mit

---
+++ test_scripts/test_angle_limit.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*-coding:utf8-*-
+import time
+from piper_sdk import *
+from math import pi
+
+def enable_piper(piper):
+    max_retries=100
+    for i in range(max_retries):
+        if piper.EnablePiper():
+            print(f"piper enabled after {i} retries.")
+            return True
+        print(f"piper trying to enable, attempt {i + 1}/{max_retries}")
+        time.sleep(0.02)
+    print(f"Failed to enable piper after {max_retries} retries.")
+    return False
+
+def config_joint_limit(piper):
+    # Config angle limit and speed limit
+    # angle limit unit:0.1degree
+    # speed limit unit:0.001rad/s
+    piper.MotorAngleLimitMaxSpdSet(6, 450, -450, int(15/180*pi*1000))
+    print("Joint motor Angle limit and Speed limit configed.")
+
+    # Config acceleration limit
+    # acceleration limit unit:0.001rad/s^2
+    piper.JointMaxAccConfig(6, 500)
+    print("Joint Acceleration limit configed.")
+
+    print(piper.GetAllMotorAngleLimitMaxSpd())
+
+def main():
+    piper = C_PiperInterface_V2("can0")
+    piper.ConnectPort()
+    time.sleep(0.03) # Time is required to read the firmware response frame; otherwise return -0x4AF.
+    print("firmware version: ", piper.GetPiperFirmwareVersion())
+    enabled_flag = enable_piper(piper)
+    if not enabled_flag:
+        return
+    
+    config_joint_limit(piper)
+
+    while True:
+        piper.MotionCtrl_2(0x01, 0x04, 0, 0xAD)
+        piper.JointMitCtrl(6,-pi/3,0,3,0.8,0)
+        print("sent moving to -1 command to joint 6")
+        time.sleep(2)
+        piper.MotionCtrl_2(0x01, 0x04, 0, 0xAD)
+        piper.JointMitCtrl(6,0,0,3,0.8,0)
+        print("sent moving to 0 command to joint 6")
+        time.sleep(2)
+        piper.MotionCtrl_2(0x01, 0x04, 0, 0xAD)
+        piper.JointMitCtrl(6,pi/3,0,3,0.8,0)
+        print("sent moving to 1 command to joint 6")
+        time.sleep(2)
+        piper.MotionCtrl_2(0x01, 0x04, 0, 0xAD)
+        piper.JointMitCtrl(6,0,0,3,0.8,0)
+        print("sent moving to 0 command to joint 6")
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
